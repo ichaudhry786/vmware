6f64f904d1ac5fe0a587b7d3f58f1b56a83ed42a
ichaudhry786 <chaudhry.ibrahim@gmail.com>
2012-07-09 17:13:11 -0400

    Updated with the class definition of the clone operation

--- vmops.py
+++ vmops.py
@@ -14,15 +14,18 @@ from psphere.errors import ObjectNotFoundError
 from psphere.client import Client
 from psphere.soap import VimFault
 from psphere.managedobjects import VirtualMachine
-
-
-
+from pysphere import VIServer
 
 
 class Vmops:
  client=None
+ server=None
  def connect(self,server_name,userid, password):
     self.client=Client(server_name,userid,password)
+
+ def connectVIServer(self,server_name,userid, password):
+     self.server=VIServer()
+     self.server.connect(server_name,userid,password)
 
  def cloneMachine(self,source_vm_name, dest_vm_name):
 
@@ -47,6 +50,7 @@ class Vmops:
         vm_clone_spec.template = False
         vm_clone_spec.location = vm_reloc_spec
         vm_clone_spec.snapshot = None
+
 
         task = vm.CloneVM_Task(folder=vm.parent, name=dest_vm_name, spec=vm_clone_spec)
 
@@ -74,24 +78,79 @@ class Vmops:
     except VimFault, e:
         print("Failed to clone %s: " % e)
         sys.exit()
+
+
+
  def startVm(self,vmname):
-    print("This is the start vm function")
+     vm=self.server.get_vm_by_name(vmname)
+     status=vm.get_status()
+     if status=="POWERED OFF":
+         vm.power_on()
+
+
  def stopVm(self, vmname):
-    print("This is to stop machine")
- def ChangeVmConfig():
-    print("This is to change VM Configuration")
+     vm=self.server.get_vm_by_name(vmname)
+     status=vm.get_status()
+     if status=="POWERED ON":
+         vm.power_off()
+
+ def stopGuest(self,vmname):
+      vm=self.server.get_vm_by_name(vmname)
+      vm.shutdown_guest()
+
+ def rebootGuest(self,vmname):
+     vm=self.server.get_vm_by_name(vmname)
+     vm.reboot_guest()
 
 
+ def changevmConfig(self,vm_name,cpuCount):
+    try:
+     new_config = self.client.create("VirtualMachineConfigSpec")
+     new_config.numCPUs = cpuCount
+     new_config.cpuHotAddEnabled=True
+     vm = VirtualMachine.get(self.client, name=vm_name)
+     print("Reconfiguring %s" % vm_name)
+     if vm.config.hardware.numCPU == cpuCount:
+        print("Not reconfiguring %s as it already has 2 CPUs" % vm_name)
+        sys.exit()
+     task = vm.ReconfigVM_Task(spec=new_config)
+     while task.info.state in ["queued", "running"]:
+      print("Waiting 5 more seconds for VM creation")
+      time.sleep(5)
+      task.update()
+
+     if task.info.state == "success":
+      elapsed_time = task.info.completeTime - task.info.startTime
+      print("Successfully reconfigured VM %s. Server took %s seconds." %
+          (vm_name, elapsed_time.seconds))
+     elif task.info.state == "error":
+      print("ERROR: The task for reconfiguring the VM has finished with"
+          " an error. If an error was reported it will follow.")
+      print("ERROR: %s" % task.info.error.localizedMessage)
+    except VimFault, e:
+     print("Failed to reconfigure %s: " % e)
+     sys.exit()
+    except ObjectNotFoundError:
+     print("ERROR: No VM found with name %s" % vm_name)
 
 
-
-def closeconnection():
+ def closeconnection():
      client.logout()
+
 
 source_vm_name = "Chef Node"
 dest_vm_name = "Chef Node Clone"
 
 x=Vmops()
+x.connectVIServer("69.33.0.216","vpxuser","Tubuai123!")
 x.connect("69.33.0.216","vpxuser","Tubuai123!")
-x.cloneMachine(source_vm_name,dest_vm_name)
-x.close()
+x.stopVm(source_vm_name)
+x.changevmConfig(source_vm_name,2)
+#x.cloneMachine(source_vm_name,dest_vm_name)
+x.startVm(source_vm_name)
+
+#x.stopGuest(source_vm_name)
+#x.rebootGuest(source_vm_name)
+
+
+#x.closeconnection()
