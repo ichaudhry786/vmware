aa1bb4327a0ad24539674cf28e99e03e84beb5ac
ichaudhry786 <chaudhry.ibrahim@gmail.com>
2012-07-12 19:16:54 -0400

    Updated with the class definition of the clone operation

--- basicOps.py
+++ basicOps.py
@@ -11,17 +11,31 @@ from psphere.client import Client
 from psphere.soap import VimFault
 from psphere.managedobjects import VirtualMachine
 from pysphere import VIServer
-from psphere import config, template
-import createvmfromtemplate
+from config import Config
+import re
+
+
 
 
 class basicOps:
 
     server=None
 
-    def connectVIServer(self,server_name,userid, password):
+    def connect(self):
+        configuration=Config()
+        server = configuration._config_value("general", "server")
+        if server is None:
+            raise ValueError("server must be supplied on command line"+"or in configuration file.")
+        username = configuration._config_value("general", "username")
+        if username is None:
+            raise ValueError("username must be supplied on command line"
+                         " or in configuration file.")
+        password = configuration._config_value("general", "password")
+        if password is None:
+            raise ValueError("password must be supplied on command line"
+                         " or in configuration file.")
         self.server=VIServer()
-        self.server.connect(server_name,userid,password)
+        self.server.connect(server,username,password)
 
     def startVm(self,vmname):
         vm=self.server.get_vm_by_name(vmname)
@@ -43,8 +57,41 @@ class basicOps:
         vm=self.server.get_vm_by_name(vmname)
         vm.reboot_guest()
 
+    def clone(self,vmname,cluster=None, datacenter=None, resourcePool=None):
+        vm=self.server.get_vm_by_name(vmname)
+        resource_pools=self.server.get_resource_pools()
+        arp=resource_pools.keys()[0]
+        new_vm=vm.clone("Chef node Clone",resourcepool=arp)
+
+    def getResourceList(self, cluster=None, datacenter=None, resourcePool=None):
+         if datacenter==None and cluster==None:
+            resource_pools=self.server.get_resource_pools()
+         elif (datacenter==None and cluster !=None):
+            clusters=self.server.get_clusters()
+            print clusters
+            keycluster=self.find_key(clusters,cluster)
+            print(keycluster)
+            resource_pools=self.server.get_resource_pools(keycluster)
+            print(resource_pools)
+            resourcePool=self.find_key(resource_pools,resourcePool,True)
+            print(resourcePool)
 
 
+
+ #               resource_pools=self.server.get_resource_pools()
+    def find_key(self,dic, val, partial=False):
+      retval=None
+      if partial==False:
+       retval=[k for k, v in dic.iteritems() if v == val][0]
+      else:
+        retval =[k for k, v in dic.iteritems() if (v.find(val)>0)][0]
+      return retval
+
+    def find_value(self,dic, key):
+     return dic[key]
+
+         #resource_pools=self.server.get_resource_pools()er,
+         #print(resource_pools.values())
 
 #source_vm_name = "Chef Server"
 #dest_vm_name = "Chef Server Clone"
